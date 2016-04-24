e0ce8dbc0c0a03f7e19627da13967b7257c42213
subliminalj <jshtz8@mail.umkc.edu>
2016-04-23 19:24:44 -0500

    added blank function headers in the main

--- files/main.cpp
+++ files/main.cpp
@@ -2,8 +2,17 @@
 
 #include "AVL_Tree.h"
 #include "Employee.h"
-
+#include "File.h"
+#include <list>
 using namespace std;
+
+void add_folder(string path, string folder_name) {}
+void delete_folder(string path, string folder_name) {}
+void add_file(string path, string file_name, int size) {}
+File get_file(string path, string file_name) {}
+list<File> get_files(string path, string file_name){}
+void delete_file(string path, string file_name){}
+
 
 int main(){
 
